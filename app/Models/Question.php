a268ed4a2311a2af9e32565af59ee89e2f2aa8ba
afsanehmh <35760286+afsanehmh@users.noreply.github.com>
2020-12-17 13:35:14 +0330

    Releationship models

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class)->latest();
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
